bbf62ef94a3df845aff591d92696bb738828a45c
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-03-27 15:44:45 +0600

    ema-jhon first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css'
-import Header from './components/Header/Header';
-import Main from './components/Main';
+import Main from './components/Main/Main';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-     <Header>Sort By Date</Header>
-     <Main></Main>
+    <Navbar></Navbar>
+    <Main></Main>
       
     </div>
   );
